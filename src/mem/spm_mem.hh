243df75a161e09368278029d80eae2ccd3e30947
Markos Horro <marcos.horro@udc.es>
2015-04-16 22:12:36 +0200

    First declaration of SPM. Size it is not declared yet, searching for the best type for it

--- src/mem/spm_mem.hh
+++ src/mem/spm_mem.hh
@@ -33,28 +33,25 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Authors: Markos Horro
+ * Authors: Markos Horro (thanks Christian Menard for the idea)
  *
- * NOTE: Thanks Christian Menard for the idea
  */
 
 /**
  * @file
- * SimpleMemory declaration, based on SimpleMemory
+ * ScratchpadMemory declaration, based on SimpleMemory
  */
 
 #ifndef __SCRATCHPAD_MEMORY_HH__
 #define __SCRATCHPAD_MEMORY_HH__
-
-#include <deque>
 
 #include "mem/abstract_mem.hh"
 #include "mem/port.hh"
 #include "params/ScratchpadMemory.hh"
 
 /**
- * The simple memory is a basic single-ported memory controller with
- * a configurable throughput and latency.
+ * The scratchpad is a basic double-ported memory controller with
+ * configurable size and latencies
  *
  * @sa  \ref gem5MemorySystem "gem5 Memory System"
  */
@@ -64,9 +61,9 @@ class ScratchpadMemory : public AbstractMemory
   private:
 
     /**
-     * Definition ScratchpadPort
+     * Definition ScratchpadSlavePort
      */
-    class ScratchpadPort : public SlavePort
+    class ScratchpadSlavePort : public SlavePort
     {
 
       private:
@@ -85,100 +82,90 @@ class ScratchpadMemory : public AbstractMemory
 
         bool recvTimingReq(PacketPtr pkt);
 
-        void recvRespRetry();
+        AddrRangeList getAddrRanges() const;
+
+    };
+
+    /**
+     * Definition ScratchpadMasterPort
+     */
+    class ScratchpadMasterPort : public MasterPort
+    {
+
+      private:
+
+        ScratchpadMemory& memory;
+
+      public:
+
+        ScratchpadPort(const std::string& _name, ScratchpadMemory& _memory);
+
+      protected:
+
+        Tick sendAtomic(PacketPtr pkt);
+
+        void sendFunctional(PacketPtr pkt);
+
+        bool sendTimingReq(PacketPtr pkt);
 
         AddrRangeList getAddrRanges() const;
 
     };
 
-    MemoryPort port;
+    /**
+     * Ports: master and slave
+     */
+    ScratchpadSlavePort slave_port; // connect with CPU
+
+    ScratchpadMasterPort master_port; // connect with main memory
 
     /**
      * Latency from that a request is accepted until the response is
      * ready to be sent.
      */
-    const Tick latency;
+    const Tick latency_read;
 
     /**
-     * Fudge factor added to the latency.
+     * Latency from that a request is sent until the response is
+     * received.
      */
-    const Tick latency_var;
+    const Tick latency_write;
 
     /**
-     * Internal (unbounded) storage to mimic the delay caused by the
-     * actual memory access. Note that this is where the packet spends
-     * the memory latency.
+     * Fudge factor added to the read latency
      */
-    std::deque<DeferredPacket> packetQueue;
+    const Tick latency_read_var;
 
     /**
-     * Bandwidth in ticks per byte. The regulation affects the
-     * acceptance rate of requests and the queueing takes place after
-     * the regulation.
+     * Fudge factor added to the write latency
      */
-    const double bandwidth;
+    const Tick latency_write_var;
 
     /**
-     * Track the state of the memory as either idle or busy, no need
-     * for an enum with only two states.
+     * Detemine the writting latency
+     *
+     * @return the latency seen by the packet requested
      */
-    bool isBusy;
+    Tick getLatencyWrite() const;
 
     /**
-     * Remember if we have to retry an outstanding request that
-     * arrived while we were busy.
-     */
-    bool retryReq;
-
-    /**
-     * Remember if we failed to send a response and are awaiting a
-     * retry. This is only used as a check.
-     */
-    bool retryResp;
-
-    /**
-     * Release the memory after being busy and send a retry if a
-     * request was rejected in the meanwhile.
-     */
-    void release();
-
-    EventWrapper<SimpleMemory, &SimpleMemory::release> releaseEvent;
-
-    /**
-     * Dequeue a packet from our internal packet queue and move it to
-     * the port where it will be sent as soon as possible.
-     */
-    void dequeue();
-
-    EventWrapper<SimpleMemory, &SimpleMemory::dequeue> dequeueEvent;
-
-    /**
-     * Detemine the latency.
+     * Detemine the reading latency
      *
      * @return the latency seen by the current packet
      */
-    Tick getLatency() const;
+    Tick getLatencyRead() const;
 
-    /** @todo this is a temporary workaround until the 4-phase code is
-     * committed. upstream caches needs this packet until true is returned, so
-     * hold onto it for deletion until a subsequent call
-     */
-    std::vector<PacketPtr> pendingDelete;
-
-    /**
-     * If we need to drain, keep the drain manager around until we're
-     * done here.
-     */
-    DrainManager *drainManager;
 
   public:
 
-    SimpleMemory(const SimpleMemoryParams *p);
-
-    unsigned int drain(DrainManager *dm);
+    ScratchpadMemory(const ScratchpadMemoryParams *p);
 
     BaseSlavePort& getSlavePort(const std::string& if_name,
                                 PortID idx = InvalidPortID);
+
+    BaseSlavePort& getMasterPort(const std::string& if_name,
+                                PortID idx = InvalidPortID);
+
     void init();
 
   protected:
@@ -194,3 +181,5 @@ class ScratchpadMemory : public AbstractMemory
 };
 
 #endif //__SCRATCHPAD_MEMORY_HH__
+
+
