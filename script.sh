e3e0c8df929c73bf3fdcb69e086056be0b6f1dce
Marcos Horro <marcos.horro@udc.es>
2015-10-27 22:53:54 +0100

    SCRIPT: fixed execution script. Have to find out an easier way to pass the parameters to the execution...

--- script.sh
+++ script.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
+
+################################################
 # TEST TIME
 #./build/X86/gem5.opt --stats-file=testmp.txt --debug-flags=PseudoInst --debug-file=test-tmp.out  configs/example/se.py --mem-size=4GB --caches -c tests/test-progs/spm/test_dir/testmp
 
+################################################
 # TEST TIME SPM
 #./build/X86/gem5.opt --stats-file=testmpspm.txt --debug-flags=PseudoInst --debug-file=test-tmp-spm-trace.out configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=32MB --spm-type-1=2 --caches -c tests/test-progs/spm/test_dir/testmp --checkpoint-dir=m5out -r 1
 
+################################################
 # TEST NO SPM 
 #./build/X86/gem5.opt --stats-file=nospm.txt --debug-flags=PseudoInst --debug-file=test-nospm-trace.out  configs/example/se.py --mem-size=4GB --caches -c tests/test-progs/spm/test_dir/testn 
 
+################################################
 # TEST With SPM
-#./build/X86/gem5.opt --stats-file=spm.txt --debug-flags=PseudoInst --debug-file=test-spm-trace.out configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=32MB --spm-type-1=1 -c tests/test-progs/spm/test_dir/test
 
-# 2 SPM: 1 SP, 2 DP
-#./build/X86/gem5.opt --stats-file=spmDP.txt --debug-flags=PseudoInst --debug-file=trace.out configs/spm/se.py --mem-size=4GB --scratchpad=2 --spm-size-1=32MB --spm-size-2=4MB --spm-type-2=2 --caches -c tests/test-progs/spm/test_dir/test
+# 5ns
+#./build/X86/gem5.opt --stats-file=spm.txt --debug-flags=PseudoInst --debug-file=test-spm-trace.out configs/spm/se.py --cpu-type=timing --mem-size=4GB --scratchpad=1 --spm-size-1=32MB --spm-type-1=1 --spm-lat-1=5ns -c tests/test-progs/spm/test_dir/test
 
+# 1ns
+#./build/X86/gem5.opt --stats-file=spm.txt --debug-flags=PseudoInst --debug-file=test-spm-trace.out configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=32MB --spm-type-1=1 --spm-lat-1=1ns -c tests/test-progs/spm/test_dir/test
+
+################################################
+# @TODO
 # 2MM
 #./build/X86/gem5.opt --stats-file=spm2mm.txt --debug-flags=PseudoInst --debug-file=trace.out configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=32MB --caches -c tests/test-progs/spm/2mm/2mm
 
