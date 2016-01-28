ba176de833848d5af71840296cf5c91ae7cf864d
Markos Horro Varela <marcos.horro@udc.es>
2016-01-28 18:00:11 +0100

    Added execution permission

--- script-2mm.sh
+++ script-2mm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ################################################
-# @TODO
 # 2MM
+
 #./build/X86/gem5.debug --stats-file=spm2mm.txt --debug-flags=PseudoInst --debug-file=trace.out configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=32MB --caches -c tests/test-progs/spm/2mm/2mm
 
 # 2mm ORG
@@ -13,6 +13,3 @@
 
 # 2mm SPM cache
 #./build/X86/gem5.debug --stats-file=2mm-spm.txt --debug-flags=PseudoInst --debug-file=2mm-spm-trace.out configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=128MB --spm-type-1=2 --caches -c tests/test-progs/spm/2mm/spm
-
-# Both in SPM
-
