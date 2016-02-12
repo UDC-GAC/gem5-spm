1e13901310a36cfeab7dcc18b81d8ea20c726376
Markos Horro Varela <marcos.horro@udc.es>
2016-02-12 18:37:03 +0100

    Fixed trmm benchmark run script

--- script-trmm.sh
+++ script-trmm.sh
@@ -3,11 +3,20 @@
 ################################################
 # trmm
 
+# Local variables
+usage="Parameters needed:\n\t* exec: executing trmm benchmarks\n\t* cache <number>: executes cachegrind tests for <number> configuration"
+error="Wrong number of parameters: 1 needed at least"
 dir=./tests/test-progs/spm/trmm
 
-echo $#
+# Script
+if [ $# = 0 ]
+then
+    echo $error
+    echo $usage
+    return
+fi
 
-if [ $# = 1 ]
+if [ $1 = "exec" ]
 then
    # 1 A in main memory, B in SPM
    ./build/X86/gem5.debug --stats-file=trmm-spm-1.txt configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=16MB --spm-type-1=1 --spm-r-lat-1=2ns --spm-w-lat-1=3ns --caches --cpu-type=timing -c tests/test-progs/spm/trmm/spm
@@ -23,13 +32,8 @@ then
 
    # 5 cache l2
    ./build/X86/gem5.debug --stats-file=trmm-no-spm.txt configs/example/se.py --mem-size=4GB --caches --cpu-type=timing -c tests/test-progs/spm/trmm/org
-fi
-
-## Cachegrind
-if [ $# > 3 ]
+elif [ $1 = "cache" ]
 then
-    valgrind --tool=cachegrind --cachegrind-out-file=trmm-cachegrind-$1.out $dir/cache$1
-    cg_annotate --auto=yes trmm-cachegrind-$1.out
-else
-    echo "Wrong number of parameters: 4 needed at least\n"
+    valgrind --tool=cachegrind --cachegrind-out-file=trmm-cachegrind-$2.out $dir/cache$2
+    cg_annotate --auto=yes trmm-cachegrind-$2.out
 fi
