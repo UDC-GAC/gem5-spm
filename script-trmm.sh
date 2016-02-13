2070e5d168bffadabb59373b314d8ede0d32164b
Markos Horro Varela <marcos.horro@udc.es>
2016-02-13 18:21:41 +0100

    Modified script for trmm becnhmark execution

--- script-trmm.sh
+++ script-trmm.sh
@@ -18,20 +18,23 @@ fi
 
 if [ $1 = "exec" ]
 then
-   # 1 A in main memory, B in SPM
+   # 1 A in main memory, B in SPM (2/3)
    ./build/X86/gem5.debug --stats-file=trmm-spm-1.txt configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=16MB --spm-type-1=1 --spm-r-lat-1=2ns --spm-w-lat-1=3ns --caches --cpu-type=timing -c tests/test-progs/spm/trmm/spm
 
-   # 2 A in main memory, B in SPM
+   # 2 A in main memory, B in SPM (2.5/3.2)
    ./build/X86/gem5.debug --stats-file=trmm-spm-2.txt configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=16MB --spm-type-1=1 --spm-r-lat-1=2.5ns --spm-w-lat-1=3.2ns --caches --cpu-type=timing -c tests/test-progs/spm/trmm/spm
 
-   # 3 A in main memory, B in SPM
+   # 3 A in main memory, B in SPM (2.2/2.5)
    ./build/X86/gem5.debug --stats-file=trmm-spm-3.txt configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=16MB --spm-type-1=1 --spm-r-lat-1=2.2ns --spm-w-lat-1=2.5ns --caches --cpu-type=timing -c tests/test-progs/spm/trmm/spm
 
-   # 4 Both in main memory: see configs/common/Options.py to check the cache configuration
-   ./build/X86/gem5.debug --stats-file=trmm-no-spm.txt configs/example/se.py --mem-size=4GB --caches --cpu-type=timing -c tests/test-progs/spm/trmm/org
+   # 4 A in main memory, B in SPM (2/2)
+   ./build/X86/gem5.debug --stats-file=trmm-spm-4.txt configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=16MB --spm-type-1=1 --spm-r-lat-1=2ns --spm-w-lat-1=2ns --caches --cpu-type=timing -c tests/test-progs/spm/trmm/spm
 
-   # 5 cache l2
-   ./build/X86/gem5.debug --stats-file=trmm-no-spm.txt configs/example/se.py --mem-size=4GB --caches --cpu-type=timing -c tests/test-progs/spm/trmm/org
+   # 5 Both in main memory: see configs/common/Options.py to check the cache configuration
+   ./build/X86/gem5.debug --stats-file=trmm-nospm-convarch.txt configs/example/se.py --mem-size=4GB --caches --cpu-type=timing -c tests/test-progs/spm/trmm/org
+
+   # 6 Using another architecture
+   ./build/X86/gem5.debug --stats-file=trmm-nospm-spmarch.txt configs/spm/se.py --mem-size=4GB --caches --cpu-type=timing -c tests/test-progs/spm/trmm/org
 elif [ $1 = "cache" ]
 then
     valgrind --tool=cachegrind --cachegrind-out-file=trmm-cachegrind-$2.out $dir/cache$2
