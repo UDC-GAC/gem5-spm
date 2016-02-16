4fd75f52088ce84a1e2924bd5e3e6a648db7092e
Markos Horro Varela <marcos.horro@udc.es>
2016-02-16 19:15:35 +0100

    Fixed script for 2mm benchmark

--- script-2mm.sh
+++ script-2mm.sh
@@ -3,13 +3,17 @@
 ################################################
 # 2MM
 
-#./build/X86/gem5.debug --stats-file=spm2mm.txt --debug-flags=PseudoInst --debug-file=trace.out configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=32MB --caches -c tests/test-progs/spm/2mm/2mm
+# 2/3
+./build/X86/gem5.debug --stats-file=2mm-spm.txt configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=16MB --spm-type-1=1 --spm-r-lat-1=2ns --spm-w-lat-1=3ns --caches --cpu-type=timing -c tests/test-progs/spm/2mm/2mm
 
-# 2mm ORG
-#./build/X86/gem5.debug --stats-file=2mm-no-spm.txt --debug-flags=PseudoInst --debug-file=2mm-trace.out configs/example/se.py --mem-size=4GB --caches -c tests/test-progs/spm/2mm/org
+# 3/5
+./build/X86/gem5.debug --stats-file=2mm-spm.txt configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=16MB --spm-type-1=1 --spm-r-lat-1=3ns --spm-w-lat-1=5ns --caches --cpu-type=timing -c tests/test-progs/spm/2mm/2m
 
-# 2mm SPM no cache
-#./build/X86/gem5.debug --stats-file=2mm-spm.txt --debug-flags=PseudoInst --debug-file=2mm-spm-trace.out configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=128MB --spm-type-1=2 -c tests/test-progs/spm/2mm/spm
+# 2/2
+./build/X86/gem5.debug --stats-file=2mm-spm.txt configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=16MB --spm-type-1=1 --spm-r-lat-1=2ns --spm-w-lat-1=2ns --caches --cpu-type=timing -c tests/test-progs/spm/2mm/2mm
 
-# 2mm SPM cache
-#./build/X86/gem5.debug --stats-file=2mm-spm.txt --debug-flags=PseudoInst --debug-file=2mm-spm-trace.out configs/spm/se.py --mem-size=4GB --scratchpad=1 --spm-size-1=128MB --spm-type-1=2 --caches -c tests/test-progs/spm/2mm/spm
+# 2mm conventional arch
+./build/X86/gem5.debug --stats-file=2mm-no-spm-convarch.txt configs/example/se.py --mem-size=4GB --caches --cpu-type=timing -ctests/test-progs/spm/2mm/org
+
+# 2mm SPM arch
+./build/X86/gem5.debug --stats-file=2mm-no-spm-spmarch.txt configs/spm/se.py --mem-size=4GB --caches --cpu-type=timing -c tests/test-progs/spm/2mm/org
