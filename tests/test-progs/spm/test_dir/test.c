472169b988e0583a6870d43d19e24e9d4db00e5a
Markos Horro Varela <marcos.horro@udc.es>
2016-04-22 21:47:53 +0200

    Testing simulatenous use of 2 spm in architecture

--- tests/test-progs/spm/test_dir/test.c
+++ tests/test-progs/spm/test_dir/test.c
@@ -21,7 +21,8 @@ int main(int argc, char** argv)
   printf("test program start\n");
   
   int *p = (int *) spm_malloc(sizeof(int)*16*KBYTE, 1);
-
+  int *s = (int *) spm_malloc(sizeof(int)*16*KBYTE, 2);
+  
   int i = 0;
 
   if (DEBUG) {
@@ -30,9 +31,10 @@ int main(int argc, char** argv)
   }
 
   m5_reset_stats(0,0);
-  for (i=0; i<16*KBYTE; i++) {
-    p[i] = i; 
-    //printf("dir: %d\n", p[i]);
+  for (i=0; i<KBYTE; i++) {
+    p[i] = i;
+    s[i] = p[i] + 1;
+    printf("dir: %d, %d\n", p[i], s[i]);
   }
   m5_dump_stats(0,0);
 
